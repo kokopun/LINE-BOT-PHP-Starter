85c3c0687b0db074b0b4333d80c38ec02ca3919a
kokopun <engiball@hotmail.com>
2017-03-23 13:00:13 +0700

    Vvvv

--- bot.php
+++ bot.php
@@ -130,19 +130,11 @@ if (!is_null($events['events'])) {
 
 	
 
-			// Make a POST Request to Messaging API to reply to sender
-			$data2 = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages2],
-			];
-			$post2 = json_encode($data2);
-			$headers2 = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post2);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers2);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
@@ -167,7 +159,16 @@ if (!is_null($events['events'])) {
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
                
+	$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
+			echo $result . "\r\n";
 		
 
 	}
